8a51024315f1b6788ecb2c28cb78833dc78eb429
Jenkins Build <no-reply@ifthenelse.org>
2025-01-04 20:56:35 -0800

    reduce workers to 2

--- distributed_caching/service_run.sh
+++ distributed_caching/service_run.sh
@@ -4,6 +4,6 @@
 gunicorn caching:app \
          --bind 0.0.0.0:9090 \
          --timeout 2592000 \
-         --workers 4 \
+         --workers 2 \
          --reload \
          --log-level 'info'
