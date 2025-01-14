150c39606923c2831313d4e340f6a434ffa2f87c
Ovais Quraishi <ovaisq@icloud.com>
2025-01-14 12:31:18 -0800

    updated build_n_deploy.sh script to mimic GitHub action  and .github/workflows/main.yml

--- ollama_ai/embedding/build_n_deploy.sh
+++ ollama_ai/embedding/build_n_deploy.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 # ©2025, Ovais Quraishi
-# NOTE: assumes that setup.config.orig, with all settings assigned
-#		except "version" is set to "version=SEMVER" exists
+# NOTE: assumes that config_vals.txt, with all settings assigned
+#		exists
 
 # Define a function for each operation
 build_docker() {
-  my_ver=$1
-  cp setup.config.orig setup.config
-  sed -i "s|SEMVER|$my_ver|" setup.config
+  cp setup.config.template setup.config
+  sed -i "s|PSQLHOST|$PSQLHOST|" setup.config
+  sed -i "s|PSQLDB|$PSQLDB|" setup.config
+  sed -i "s|PSQLUSER|$PSQLUSER|" setup.config
+  sed -i "s|PSQLPASS|$PSQLPASS|" setup.config
+  sed -i "s|OLLAMA_HOST_URL|$OLLAMA_HOST_URL|" setup.config
+  sed -i "s|OLLAMA_LLM|$OLLAMA_LLM|" setup.config
+  sed -i "s|SEMVER|$SEMVER|" setup.config
   ./build_docker.py
 }
 
 push_image() {
-  my_ver=$1
-  service_name=$2
-  docker_host_uri=$3
 
-  echo "docker tag ${service_name}:${my_ver} ${docker_host_uri}/${service_name}:${my_ver}"
-  docker tag "${service_name}:${my_ver}" "${docker_host_uri}/${service_name}:${my_ver}"
-  echo "docker push ${docker_host_uri}/${service_name}:${my_ver}"
-  docker push "${docker_host_uri}/${service_name}:${my_ver}"
+  echo "docker tag ${SERVICE_NAME}:${SEMVER} ${DOCKER_HOST_URI}/${SERVICE_NAME}:${SEMVER}"
+  docker tag "${SERVICE_NAME}:${SEMVER}" "${DOCKER_HOST_URI}/${SERVICE_NAME}:${SEMVER}"
+  echo "docker push ${DOCKER_HOST_URI}/${SERVICE_NAME}:${SEMVER}"
+  docker push "${DOCKER_HOST_URI}/${SERVICE_NAME}:${SEMVER}"
 }
 
 apply_kubernetes() {
-  my_ver=$1
-  cp deployment.yaml.orig deployment.yaml
-  sed -i "s|SEMVER|$my_ver|" deployment.yaml
+
+  #cp deployment.yaml.orig deployment.yaml
+  sed -i "s|SEMVER|$SEMVER|" deployment.yaml
+  sed -i "s|DOCKER_HOST_URI|$DOCKER_HOST_URI|" deployment.yaml
   kubectl -n ollamagpt apply -f deployment.yaml
   kubectl -n ollamagpt apply -f service.yaml
 }
 
-# Read version from ver.txt
-my_ver=$(<ver.txt)
-
-docker_host_uri="jenkins-node-1:5000"
-service_name="summarize-web-pages"
+# Read config
+source config_vals.txt
 
 # Call the functions with the version as an argument
-build_docker $my_ver
-push_image $my_ver $service_name $docker_host_uri
-apply_kubernetes $my_ver
+build_docker $SEMVER
+push_image $SEMVER $SERVICE_NAME $DOCKER_HOST_URI
+apply_kubernetes $SEMVER
