1445d4db39a2964b5a03272f92086b9d12d22073
Jenkins Build <no-reply@ifthenelse.org>
2025-03-14 21:35:07 -0700

    Kubernetes deployment and service

---
+++ time_zone_converter/build_docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+release_name="timezone-converter"
+repository_host="jenkins-node-1:5000"
+release_ver="0.1.0"
+docker build -t "$release_name:$release_ver" .
+docker tag "$release_name:$release_ver" "$repository_host/$release_name:$release_ver"
+docker push "$repository_host/$release_name:$release_ver"
