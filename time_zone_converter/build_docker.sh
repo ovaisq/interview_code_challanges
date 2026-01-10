392a203037a3e38feb465f6e056148ae509326f2
Jenkins Build <no-reply@ifthenelse.org>
2026-01-10 15:14:29 -0800

    v0.4.0

--- time_zone_converter/build_docker.sh
+++ time_zone_converter/build_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 release_name="timezone-converter"
 repository_host="jenkins-node-1:5000"
-release_ver="0.3.0"
+release_ver="0.4.0"
 docker build -t "$release_name:$release_ver" .
 docker tag "$release_name:$release_ver" "$repository_host/$release_name:$release_ver"
 docker push "$repository_host/$release_name:$release_ver"
